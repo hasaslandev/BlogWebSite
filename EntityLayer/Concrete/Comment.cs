f0b702d68a16de375c3adaeb0074efa8de1ec9fb
hasaslandev <126229388+hasaslandev@users.noreply.github.com>
2023-03-09 12:51:36 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentID { get; set; }
+        [StringLength(50)]
+
+        public string UserName { get; set; }
+        [StringLength(50)]
+
+        public string Mail { get; set; }
+        [StringLength(500)]
+        public string CommentText { get; set; }
+
+        public int BlogID { get; set; }
+        public virtual Blog Blogs { get; set; }
+    }
+}
